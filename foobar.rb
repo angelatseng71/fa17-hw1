3caf46f3788b55a8b67e15754ca1c94e7d8ec8bc
Angela Tseng <angelatseng@berkeley.edu>
2017-09-19 15:22:25 -0700

    hw1 submission

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,13 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    arr = a.map { |e| (e.to_i) + 2 }
+    arr.delete_if { |e| e % 2 != 0 }
+    arr.uniq
+    arr.delete_if { |e| e > 10 }
+    x = 0
+    arr.map { |e| x += e  }
+    x
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  answer = (a + b) * (a + b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  arr = a.map { |b| b + 1  }
+  arr.sort
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  full_name = first_name + " " + last_name
 end
 
 def blockin_time(a)
